8d5baea8c6e962f34b624c9f76cd63a627e8dd18
Peter Johnston <peterwj@cs.stanford.edu>
2019-12-08 10:30:52 -0800

    problem 2 part 2

--- p02/main.hs
+++ p02/main.hs
@@ -30,6 +30,26 @@ runComputer ip memory = let op = memory!!ip in
          let newMemory = applyOperator ip memory operator in
            runComputer (ip+4) newMemory
 
+reportResult :: [Int] -> Int
+reportResult memory = (runComputer 0 memory)!!0
+
+initalMemory :: [Int]
+initalMemory = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,10,23,2,10,23,27,1,27,6,31,1,13,31,35,1,13,35,39,1,39,10,43,2,43,13,47,1,47,9,51,2,51,13,55,1,5,55,59,2,59,9,63,1,13,63,67,2,13,67,71,1,71,5,75,2,75,13,79,1,79,6,83,1,83,5,87,2,87,6,91,1,5,91,95,1,95,13,99,2,99,6,103,1,5,103,107,1,107,9,111,2,6,111,115,1,5,115,119,1,119,2,123,1,6,123,0,99,2,14,0,0]
+
+generateMemory noun verb = (head initalMemory):noun:verb:(drop 3 initalMemory)
+
+targetResult :: Int
+targetResult = 19690720
+
+inputRange :: [Int]
+inputRange = [0..99]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+
+guessInput :: [Int] -> Int -> (Int, Int)
+guessInput memory desiredResult = head $ filter (
+  \(noun, verb) -> reportResult (generateMemory noun verb) == desiredResult
+                                         ) (cartProd inputRange inputRange)
+
 testPart1 :: [Int] -> [Int] -> Test
 testPart1 inputMemory expectedMemory = TestLabel "test" $ TestCase (assertEqual "" expectedMemory (runComputer 0 inputMemory))
 
