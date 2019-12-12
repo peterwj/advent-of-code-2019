f479adef88f08a22b9d602bb4d5b692f162bdea3
Peter Johnston <peterwj@cs.stanford.edu>
2019-12-11 22:51:11 -0800

    day 4 part 2

--- p04/main.hs
+++ p04/main.hs
@@ -12,18 +12,37 @@ char2Int :: Char -> Int
 char2Int c = read [c]
 
 digitsIncreaseHelper :: DigitPair -> Bool
-digitsIncreaseHelper (d1, d2) = (char2Int d1) <= (char2Int d2)
+digitsIncreaseHelper (d1, d2) = char2Int d1 <= char2Int d2
 
 digitsIncrease :: Password -> Bool
 digitsIncrease password = let pairs = adjacentDigits password in
-  all (digitsIncreaseHelper) pairs
+  all digitsIncreaseHelper pairs
 
 twoAdjacentNumbersSame :: Password -> Bool
 twoAdjacentNumbersSame password = let pairs = adjacentDigits password in
-    any (\(d1, d2) -> d1 == d2) pairs
+    any (uncurry ( == )) pairs
 
-checkPassword :: Password -> Bool
-checkPassword password = twoAdjacentNumbersSame password && digitsIncrease password
+groupFoldFn :: [String] -> Char -> [String]
+groupFoldFn result c
+  | null result = [[c]]
+  | otherwise = let lastGroup = last result in
+      let lastGroupsChar = head lastGroup in
+        if c == lastGroupsChar
+        then init result ++ [lastGroup ++ [c]]
+        else result ++ [[c]]
 
-solvePuzzle :: Int
-solvePuzzle = length $ filter checkPassword puzzleInput
+groupDigits :: Password -> [String]
+groupDigits password = foldl groupFoldFn [] $ show password
+
+groupsOfTwo :: Password -> Bool
+groupsOfTwo password = let groups = groupDigits password in
+  any (\group -> 2 == length group) groups
+
+checkPasswordPart1 :: Password -> Bool
+checkPasswordPart1 password = twoAdjacentNumbersSame password && digitsIncrease password
+
+checkPasswordPart2 :: Password -> Bool
+checkPasswordPart2 password = digitsIncrease password && groupsOfTwo password
+
+solvePuzzle :: (Password -> Bool) -> Int
+solvePuzzle checkPasswordFn = length $ filter checkPasswordFn puzzleInput
