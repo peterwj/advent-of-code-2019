1122f39655a1fe930cfc90c50783ecd42b825e99
Peter Johnston <peterwj@cs.stanford.edu>
2019-12-16 09:06:47 -0800

    WIP day 6 part 1

--- p06/main.hs
+++ p06/main.hs
@@ -1,20 +1,19 @@
 module Day06 where
+
 import Data.List.Split
 import Data.Map
 import Data.Maybe
+import Data.Tree
 
 type Object = String
 
 data ParsedOrbit = ParsedOrbit { poSatellite :: Object, poCenter :: Object} deriving Show
 
+type OrbitTree = Tree Object
+
 parseLine :: String -> ParsedOrbit
 parseLine line = let parts = splitOn ")" line in
   ParsedOrbit (parts!!1) (head parts)
-
-data OrbitNode = OrbitNode {
-  center :: Object,
-  satellites :: [OrbitNode]
-  } deriving Show
 
 buildOrbitMapFoldFn :: Map Object [Object] -> ParsedOrbit -> Map Object [Object]
 buildOrbitMapFoldFn result orbit = let key = poCenter orbit in
@@ -26,14 +25,35 @@ buildOrbitMapFoldFn result orbit = let key = poCenter orbit in
 buildOrbitMap :: [ParsedOrbit] -> Map Object [Object]
 buildOrbitMap = Prelude.foldl buildOrbitMapFoldFn empty
 
-buildOrbitTreeRec :: Map Object [Object] -> Object -> OrbitNode
+buildOrbitTreeRec :: Map Object [Object] -> Object -> OrbitTree
 buildOrbitTreeRec orbitMap object = let val = Data.Map.lookup object orbitMap in
   if isNothing val
-  then OrbitNode object []
-  else OrbitNode object $ Prelude.map (buildOrbitTreeRec orbitMap) (fromJust val)
+  then Node object []
+  else Node object $ Prelude.map (buildOrbitTreeRec orbitMap) (fromJust val)
 
-buildOrbitTree :: Map Object [Object] -> OrbitNode
+buildOrbitTree :: Map Object [Object] -> OrbitTree
 buildOrbitTree orbitMap = buildOrbitTreeRec orbitMap "COM"
+
+
+--foldFn needle acc tree = let recResult = findDepthInTree (Just $ 1 + fromJust acc) tree needle in
+--  if isNothing recResult then Nothing
+--  else error "asdf"
+--
+--findDepthInTree :: Maybe Int -> OrbitTree -> Object -> Maybe Int
+--findDepthInTree depthSoFar tree needle = if rootLabel tree == needle
+--  then Just 0
+--  else if subForest tree == []
+--  then Nothing
+--  else Prelude.foldl (
+--  foldFn needle
+--  ) depthSoFar (subForest tree)
+
+foldFn :: Object -> [Maybe Int] -> Maybe Int
+foldFn = error "asdf"
+
+computeParentOrbits :: OrbitTree -> Object -> Maybe Int
+--computeParentOrbits = findDepthInTree $ Just 0
+computeParentOrbits = foldTree foldFn
 
 smallInput = [
   "COM)B",
